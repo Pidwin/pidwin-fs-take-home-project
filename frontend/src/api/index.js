c60b1c8df9dd4f4cdb7a13e9ff08a75028ce2b75
Dustin Zeigler <dustin.zeigler@live.com>
2024-07-16 16:03:15 -0500

    implement @reduxjs/toolkits' configureStore
    - cleanup spacing
    - package @babel/plugin-proposal-private-property-in-object to suppress local warning that will likely never go away.

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -3,13 +3,11 @@ import axios from "axios";
 const API = axios.create({ baseURL: "http://localhost:5000" });
 API.interceptors.request.use((req) => {
   if (localStorage.getItem("profile")) {
-    req.headers.Authorization = `Bearer ${JSON.parse(localStorage.getItem("profile")).token
-      }`;
+    req.headers.Authorization = `Bearer ${JSON.parse(localStorage.getItem("profile")).token}`;
   }
   return req;
 });
 
 export const login = (formData) => API.post("/api/user/login", formData);
 export const signUp = (formData) => API.post("/api/user/signup", formData);
-export const changePassword = (formData) =>
-  API.post("/api/user/changePassword", formData);
+export const changePassword = (formData) => API.post("/api/user/changePassword", formData);
